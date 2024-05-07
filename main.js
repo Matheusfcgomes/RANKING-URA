aec3abe234691dae5d0e9d8183cd88128707842f
Matheus Fernandes <matheusfcosta.gomes@gmail.com>
2024-05-06 21:31:44 -0300

    Feat - Tornar cronômetro funcional

--- main.js
+++ main.js
@@ -1,10 +1,63 @@
-// CARREGANDO NOVAS PAGES (use o mesmo ID para os botões das outras páginas para reaproveitar esse trecho)
-const novoJogador_button = document.getElementById('newPlayer-button')
-novoJogador_button.addEventListener('click', function() {
-    window.location.href = 'cadastro.html';
-})
+// CARREGANDO NOVAS PAGES
 
 const ranking_button = document.getElementById('ranking-button')
-ranking_button.addEventListener('click', function() {
+ranking_button.addEventListener('click', () => {
     window.location.href = 'ranking.html';
+})
+
+// CRONÔMETRO
+
+const segundos_text = document.getElementById('segundos');
+const minutos_text = document.getElementById('minutos');
+var segundos = 0;
+var minutos = 0;
+total_time = 0
+var timer_state = "parado";
+
+const iniciar_button = document.getElementById('timerStartStop-button');
+iniciar_button.addEventListener('click', () => {
+    if (timer_state == "parado"){
+        timer_state = "contando";
+        iniciar_button.innerHTML = "PAUSAR";
+        cronometrar = setInterval( () => {
+            total_time++;
+            segundos = segundos_updater(segundos);
+            minutos = minutos_updater(total_time);
+            segundos_text.innerHTML = segundos;
+            minutos_text.innerHTML = minutos;
+        },1000)
+    }
+    else {
+        timer_state = "parado";
+        iniciar_button.innerHTML = "INICIAR";
+        clearInterval(cronometrar);
+    }
+})
+
+function segundos_updater(segundos) {
+    segundos++;
+    if(segundos > 59){
+        segundos = 0;
+    }
+    if(segundos < 10){
+        segundos = '0' + segundos;
+    }
+    return segundos;
+}
+
+function minutos_updater(total_time) {
+    minutos = Math.floor(total_time/60);
+    if(minutos < 10){
+        minutos = '0' + minutos;
+    }
+    return minutos;
+}
+
+const zerar_button = document.getElementById('timerReset-button');
+zerar_button.addEventListener('click', () => {
+    segundos = 0;
+    minutos = 0;
+    total_time = 0;
+    segundos_text.innerHTML = '0' + segundos;
+    minutos_text.innerHTML = '0' + segundos;
 })
