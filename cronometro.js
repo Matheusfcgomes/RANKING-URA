daf3792ba399fe74ec33fd5d668ea7f7768d5bd3
Matheus Fernandes <matheusfcosta.gomes@gmail.com>
2024-05-08 20:08:20 -0300

    Feat - Implementar verificação para dados vazios

--- cronometro.js
+++ cronometro.js
@@ -67,24 +67,82 @@ zerar_button.addEventListener('click', () => {
 const cadastrar_button = document.getElementById('cadastro-button');
 cadastrar_button.addEventListener('click', () => {
     
-    minutos = Number(minutos);
-    segundos = Number(segundos);
+    // VERIFICA SE OS DADOS NÃO ESTÃO VAZIOS
+    if(
+        document.getElementById('participante').value.length > 0 &&
+        document.getElementById('email').value.length > 0 &&
+        document.getElementById('telefone').value.length > 0 &&
+        total_time > 0
+    )
+    {
+        minutos = Number(minutos);
+        segundos = Number(segundos);
     
-    const dados = {
-        nome: document.getElementById('participante').value,
-        email: document.getElementById('email').value,
-        telefone: document.getElementById('telefone').value,
-        tempo_total: total_time,
-        minutos: minutos,
-        segundos: segundos
-    };
+        const dados = {
+            nome: document.getElementById('participante').value,
+            email: document.getElementById('email').value,
+            telefone: document.getElementById('telefone').value,
+            tempo_total: total_time,
+            minutos: minutos,
+            segundos: segundos
+        };
 
-    fetch('http://localhost:3000/ranking', {
-        method: 'POST',
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body: JSON.stringify(dados)
-    })
-    location.reload();
+        fetch('http://localhost:3000/ranking', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(dados)
+        })
+        location.reload();
+    }
+
+    // AUSENCIA DE DADOS
+    if(
+        document.getElementById('participante').value.length == 0 ||
+        document.getElementById('email').value.length == 0 ||
+        document.getElementById('telefone').value.length == 0
+    )
+    {
+        alert("Por favor preencha os dados do participante!");
+    }
+
+    if(total_time == 0){
+        alert("Por favor inicie a cronometragem!");
+    }
 })
+
+// ATUALIZANDO RECORDE
+fetch('http://localhost:3000/ranking').then(function (response) {
+    if (response.ok) {
+        return response.json();
+    } else {
+        return Promise.reject(response);
+    }
+}).then(function (data) {
+
+    const participantes = data.map(participante => ({
+        tempo_total: participante.tempo_total,
+        minutos: participante.minutos,
+        segundos: participante.segundos,
+    }));
+
+    // FORMATANDO MINUTOS E SEGUNDOS NO MODELO 00:00
+    for(let i=0; i<participantes.length; i++){
+        if(participantes[i].minutos < 10){
+            participantes[i].minutos = '0' + participantes[i].minutos
+        }
+        if(participantes[i].segundos < 10){
+            participantes[i].segundos = '0' + participantes[i].segundos
+        }
+    }
+
+    // ORDENANDO POR TEMPO
+    participantes.sort((a, b) => a.tempo_total - b.tempo_total)
+
+    const recorde_show = document.getElementById('recorde');
+    recorde_show.innerHTML = participantes[0].minutos + ':' + participantes[0].segundos;
+
+}).catch(function (err) {   
+    console.warn('Algo deu errado!' , err);
+});
